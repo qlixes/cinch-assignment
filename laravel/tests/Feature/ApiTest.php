d42b8652343dc8d622e186448f78022d4d4d4883
Antony A <qlixess@gmail.com>
2025-03-02 18:24:27 +0700

    feat: add simple feature test

--- laravel/tests/Feature/ApiTest.php
+++ laravel/tests/Feature/ApiTest.php
@@ -4,6 +4,54 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Pagination\Paginator;
 use Tests\TestCase;
 
-class ApiTest extends TestCase {}
+class ApiTest extends TestCase
+{
+    function test_api_has_pagination()
+    {
+        $response = $this->post("/api/products");
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            "code",
+            "message",
+            "data" => [
+                [
+                    "id",
+                    "sku",
+                    "name",
+                    "description",
+                    "product_attributes" => [
+                        [
+                            "id",
+                            "color",
+                            "size",
+                        ]
+                    ],
+                    "product_prices" => [
+                        [
+                            "id",
+                            "price",
+                            "region_name",
+                            "rental_period_name",
+                            "product_name",
+                        ]
+                    ],
+                ],
+            ],
+            "links",
+            "meta"  => [
+                "current_page",
+                "from",
+                "last_page",
+                "links",
+                "path",
+                "per_page",
+                "to",
+                "total",
+            ],
+        ]);
+    }
+}
