07e29789b0c8526f8891996582b811b6114038c0
Antony A <qlixess@gmail.com>
2025-03-09 16:51:10 +0700

    clean: remove unused model

--- laravel/database/seeders/ProductAttributeSeed.php
+++ laravel/database/seeders/ProductAttributeSeed.php
@@ -13,22 +13,22 @@ class ProductAttributeSeed extends Seeder
     public function run(): void
     {
         DB::table('product_attributes')->insert([
-            ['product_id' => 1, 'color_id' => 1, 'size_id' => 4],
-            ['product_id' => 1, 'color_id' => 2, 'size_id' => 4],
-            ['product_id' => 2, 'color_id' => 2, 'size_id' => 4],
-            ['product_id' => 2, 'color_id' => 2, 'size_id' => 6],
-            ['product_id' => 3, 'color_id' => 1, 'size_id' => 4],
-            ['product_id' => 3, 'color_id' => 2, 'size_id' => 4],
-            ['product_id' => 3, 'color_id' => 3, 'size_id' => 4],
-            ['product_id' => 3, 'color_id' => 1, 'size_id' => 5],
-            ['product_id' => 3, 'color_id' => 2, 'size_id' => 5],
-            ['product_id' => 3, 'color_id' => 3, 'size_id' => 5],
-            ['product_id' => 3, 'color_id' => 1, 'size_id' => 7],
-            ['product_id' => 3, 'color_id' => 2, 'size_id' => 7],
-            ['product_id' => 3, 'color_id' => 3, 'size_id' => 7],
-            ['product_id' => 4, 'color_id' => 1, 'size_id' => 4],
-            ['product_id' => 4, 'color_id' => 2, 'size_id' => 4],
-            ['product_id' => 4, 'color_id' => 3, 'size_id' => 4],
+            ['product_id' => 1, 'attribute_id' => 1, 'attribute_value_id' => 2],
+            ['product_id' => 1, 'attribute_id' => 2, 'attribute_value_id' => 4],
+            ['product_id' => 1, 'attribute_id' => 2, 'attribute_value_id' => 6],
+            ['product_id' => 2, 'attribute_id' => 1, 'attribute_value_id' => 2],
+            ['product_id' => 2, 'attribute_id' => 2, 'attribute_value_id' => 4],
+            ['product_id' => 2, 'attribute_id' => 2, 'attribute_value_id' => 6],
+            ['product_id' => 3, 'attribute_id' => 1, 'attribute_value_id' => 1],
+            ['product_id' => 3, 'attribute_id' => 1, 'attribute_value_id' => 2],
+            ['product_id' => 3, 'attribute_id' => 2, 'attribute_value_id' => 4],
+            ['product_id' => 3, 'attribute_id' => 2, 'attribute_value_id' => 5],
+            ['product_id' => 3, 'attribute_id' => 2, 'attribute_value_id' => 7],
+            ['product_id' => 4, 'attribute_id' => 1, 'attribute_value_id' => 1],
+            ['product_id' => 4, 'attribute_id' => 1, 'attribute_value_id' => 3],
+            ['product_id' => 4, 'attribute_id' => 2, 'attribute_value_id' => 4],
+            ['product_id' => 4, 'attribute_id' => 2, 'attribute_value_id' => 5],
+            ['product_id' => 4, 'attribute_id' => 2, 'attribute_value_id' => 6],
         ]);
     }
 }
